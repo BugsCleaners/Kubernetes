63d7242a5d70ffda8896f3f42131dde4cc683564
BugsCleaners <bugscleaners@gmail.com>
2022-06-27 13:59:47 +0300

    add

---
+++ New - ConfigMap/application_properites/src/test/java/com/reading_from_app_properites/application_properites/ApplicationProperitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reading_from_app_properites.application_properites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationProperitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
