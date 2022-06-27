59ce548d4d6fceab2778ccbbcbb52ecae0975b7e
BugsCleaners <bugscleaners@gmail.com>
2022-06-27 14:24:01 +0300

    add

---
+++ New - Secret/application_properites/src/main/java/com/reading_from_app_properites/application_properites/ApplicationProperitesApplication.java
@@ -0,0 +1,13 @@
+package com.reading_from_app_properites.application_properites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProperitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationProperitesApplication.class, args);
+	}
+
+}
---
+++ New - Secret/application_properites/src/main/java/com/reading_from_app_properites/application_properites/Controller/GetSetting.java
@@ -0,0 +1,29 @@
+package com.reading_from_app_properites.application_properites.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@PropertySources(value = {@PropertySource("classpath:application.properties")})
+@RestController
+@RequestMapping(path = "/")
+public class GetSetting {
+
+
+
+    @Value("${valuesecret}")
+    private String valuesecret;
+
+
+
+
+    @GetMapping("reading")
+    public String Select()
+    {
+        return "secret: "+valuesecret;
+    }
+
+
+}
